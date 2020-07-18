8b66a40ea4a8dac9e93080800da4a70e996b7f4b
Nateekarn Jongeair <60143222@g.cmru.ac.th>
2020-07-18 14:02:11 +0700

    เ

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return "สวัสดี";
+    }
+
+    public function greeting($name)
+    {
+        return "สวัสดี $name";
+    }
+    public function calcculator($num1,$num2,$num3)
+    {
+        return "ผลรวมของเลข $num1,$num2 และ $num3 คือ " . ($num1+$num2+$num3) . "นะจ๊ะ";
+  }
+    public function index()
+    {
+        return "สวัสดีจากหน้า User index";
+    }
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/user/{name}', function ($name) {
+//     return "สวัสดี $name";
+// });
+
+// Route::get("/บวกเลย/{num1}/{num2}", function ($num1, $num2) {
+//     return $num1 + $num2;
+// });
+
+// Route::get("/showdetail/{Nateekarn}/{age}/{Na}", function ($Nateekarn,$age,$At) {
+//     return "สวัสดี $Nateekarn คุณมีอายุ $age ปี และคุณพูดว่า $At";
+
+// Route::get('/','HomeController@index');
+// Route::get('/greeting/{name}','HomeController@greeting');
+
+// Route::get("/บวกเลข/{num1}/{num2}/{num3}",'HomeController@calcculator');
+
+Route::get('/user', "UserController@index");
